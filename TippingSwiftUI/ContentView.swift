9c4c892b3b32cbf4e01a86fbc0975c0f7d982d9f
Soopin Kim <spkim8901@gmail.com>
2024-08-03 10:04:34 -0700

    initial project

---
+++ TippingSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TippingSwiftUI
+//
+//  Created by SOOPIN KIM on 2024-08-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TippingSwiftUI/TippingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TippingSwiftUIApp.swift
+//  TippingSwiftUI
+//
+//  Created by SOOPIN KIM on 2024-08-03.
+//
+
+import SwiftUI
+
+@main
+struct TippingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
